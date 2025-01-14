75342067d5609e3fb50fc2300a31622f577c168f
Carlo Elvisi <carloelvisi01@outlook.it>
2025-01-14 17:00:42 +0100

    fix show

--- controllers/filmController.js
+++ controllers/filmController.js
@@ -1,46 +1,39 @@
-const connection = require('../db/conn.js');
+const connection = require("../db/conn.js");
 
 // Index
-function index  (req, res)  {
-    const sql = 'SELECT * FROM movies';
-    connection.query(sql, (err, results) => {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
-        }
-        res.json(results);
-    });
-};
+function index(req, res) {
+  const sql = "SELECT * FROM movies";
+  connection.query(sql, (err, results) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json(results);
+  });
+}
 function show(req, res) {
-    const movieId = req.params.id; 
+  const movieId = req.params.id;
 
-   
-    const sqlMovie = `
-        SELECT movies.*, reviews.vote 
-        FROM movies
-        INNER JOIN reviews ON movies.id = reviews.movie_id
-        WHERE movies.id = ?;
-    `; 
+  const sqlMovie = `SELECT movies.*, reviews.vote 
+                      FROM movies 
+                      INNER JOIN reviews ON movies.id = reviews.movie_id
+                      WHERE movies.id = ?`;
 
-    connection.query(sqlMovie, [movieId], (err, results) => { 
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
-        }
+  connection.query(sqlMovie, [movieId], (err, results) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
 
-        if (results.length === 0) {
-         
-            res.status(404).json({ error: 'Film non trovato o senza recensioni' });
-            return;
-        }
+    if (results.length === 0) {
+      res.status(404).json({ error: "Film non trovato" });
+      return;
+    }
 
-        const [movie] = results;
+    const movie = results[0];
 
-      
-        res.json(movie);
-    });
+    res.json(movie);
+  });
 }
 
-
-
-module.exports = { index, show};
+module.exports = { index, show };
